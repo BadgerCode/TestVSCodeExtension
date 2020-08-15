27ab055296a42902b1c2dd65dc609907e963c274
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-08-15 14:52:52 +0100

    Adds another command

--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,17 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+
+
+	let anotherCommand = vscode.commands.registerCommand('testextension.anotherCommand', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('This is another command!');
+	});
+
+	context.subscriptions.push(anotherCommand);
 }
 
 // this method is called when your extension is deactivated
