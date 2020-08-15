1ca61bf2732c951c2a4e667364c180366a2045a7
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-08-15 15:07:21 +0100

    Adds sample sub-menu to the 'Explorer' menu (file menu)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NodeDependenciesProvider } from './NodeDependenciesProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -32,7 +33,14 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(anotherCommand);
+
+	if (vscode.workspace.rootPath != undefined) {
+		vscode.window.registerTreeDataProvider(
+			'nodeDependencies',
+			new NodeDependenciesProvider(vscode.workspace.rootPath)
+		);
+	}
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
